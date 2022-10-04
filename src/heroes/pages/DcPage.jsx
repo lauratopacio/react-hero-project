ea26fd35b81a0941af7a8e7c4224e7928c8bca76
Laura Valdez <laura.valdez@argano.com>
2022-10-04 13:31:38 -0600

    primer commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,14 @@
+import { HeroList } from "../components/HeroList";
+
+export const DcPage = () => {
+  return (
+    <>
+    <h1>
+        Dc page
+    </h1>
+    <HeroList publisher='DC Comics' />
+    </>
+    
+  )
+}
+
